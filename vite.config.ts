2f347da3c8eba74740fb96586e8cbec9e65b5af4
Joel Guedes <153196370+JoelG-007@users.noreply.github.com>
2025-11-21 19:14:30 +0530

    Fix quotes in base path of vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/Wellness-Forever/',   // 21-11-2025 / 18.57
+  base: "/Wellness-Forever/",   // 21-11-2025 / 18.57
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
